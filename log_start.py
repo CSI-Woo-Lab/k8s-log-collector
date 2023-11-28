8892335e09d4cdb1ebfe5525c442d4e0e0549a05
JangMinGeun <dltnstlsqnf@gmail.com>
2023-11-28 10:20:12 +0000

    config modified

--- log_start.py
+++ log_start.py
@@ -41,7 +41,7 @@ spec:
         ports:
         - containerPort: 9000
           protocol: TCP
-        image: jangmingeun/k8s_log_collector:3.0.0
+        image: jangmingeun/k8s_log_collector:3.1.0
         imagePullPolicy: Always
         command: ["/bin/sh", "-c"]
         args:
