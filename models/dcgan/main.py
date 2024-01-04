a6ce2159645c610413e60acdf7c319ebf7d97d5f
JangMinGeun <dltnstlsqnf@gmail.com>
2024-01-04 11:33:40 +0000

    dcgan coco added

--- models/dcgan/main.py
+++ models/dcgan/main.py
@@ -45,8 +45,8 @@ x = Logger("dcgan", opt.batch_size)
 
 def collate_fn(batch):
   return {
-      'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
-      'labels': torch.tensor([x['labels'] for x in batch])
+      'pixel_values': torch.stack([x for x in batch]),
+      'labels': torch.tensor([x for x in batch])
 }
 ######### MINGEUN ###########
 
